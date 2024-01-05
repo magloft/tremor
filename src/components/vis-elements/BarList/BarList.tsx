c5b1ca98f46fb42c98783401a7c3eb78f710df3e
edwin satya <edwinsatyayudistira@gmail.com>
2024-01-05 15:36:51 +0700

    fix(UI): Add new props (showDivide, separateValue) for customization barlist

--- src/components/vis-elements/BarList/BarList.tsx
+++ src/components/vis-elements/BarList/BarList.tsx
@@ -9,7 +9,7 @@ import {
   ValueFormatter,
 } from "lib";
 import { colorPalette } from "lib/theme";
-import React from "react";
+import React, { Fragment } from "react";
 
 const makeBarListClassName = makeClassName("BarList");
 
@@ -40,6 +40,8 @@ export interface BarListProps extends React.HTMLAttributes<HTMLDivElement> {
   valueFormatter?: ValueFormatter;
   color?: Color;
   showAnimation?: boolean;
+  showDivide?: boolean;
+  separateValue?: boolean;
 }
 
 const BarList = React.forwardRef<HTMLDivElement, BarListProps>((props, ref) => {
@@ -48,6 +50,8 @@ const BarList = React.forwardRef<HTMLDivElement, BarListProps>((props, ref) => {
     color,
     valueFormatter = defaultValueFormatter,
     showAnimation = false,
+    showDivide = false,
+    separateValue = true,
     className,
     ...other
   } = props;
@@ -72,105 +76,139 @@ const BarList = React.forwardRef<HTMLDivElement, BarListProps>((props, ref) => {
           const Icon = item.icon;
 
           return (
-            <div
-              key={item.key ?? item.name}
-              className={tremorTwMerge(
-                makeBarListClassName("bar"),
-                // common
-                "flex items-center rounded-tremor-small bg-opacity-30",
-                rowHeight,
-                item.color || color
-                  ? getColorClassNames(item.color ?? (color as Color), colorPalette.background)
-                      .bgColor
-                  : "bg-tremor-brand-subtle dark:bg-dark-tremor-brand-subtle dark:bg-opacity-30",
-                idx === data.length - 1 ? spacing.none.marginBottom : spacing.sm.marginBottom,
-              )}
-              style={{
-                width: `${widths[idx]}%`,
-                transition: showAnimation ? "all 1s" : "",
-              }}
-            >
-              <div className={tremorTwMerge("absolute max-w-full flex", spacing.sm.left)}>
-                {Icon ? (
-                  <Icon
-                    className={tremorTwMerge(
-                      makeBarListClassName("barIcon"),
-                      // common
-                      "flex-none",
-                      // light
-                      "text-tremor-content",
-                      // dark
-                      "dark:text-dark-tremor-content",
-                      sizing.lg.height,
-                      sizing.lg.width,
-                      spacing.md.marginRight,
-                    )}
-                  />
-                ) : null}
-                {item.href ? (
-                  <a
-                    href={item.href}
-                    target={item.target ?? "_blank"}
-                    rel="noreferrer"
-                    className={tremorTwMerge(
-                      makeBarListClassName("barLink"),
-                      // common
-                      "whitespace-nowrap hover:underline truncate text-tremor-default",
-                      // light
-                      "text-tremor-content-emphasis",
-                      // dark
-                      "dark:text-dark-tremor-content-emphasis",
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ) : (
-                  <p
-                    className={tremorTwMerge(
-                      makeBarListClassName("barText"),
-                      // common
-                      "whitespace-nowrap truncate text-tremor-default",
-                      // light
-                      "text-tremor-content-emphasis",
-                      // dark
-                      "dark:text-dark-tremor-content-emphasis",
-                    )}
-                  >
-                    {item.name}
-                  </p>
+            <Fragment key={item.key ?? item.name}>
+              <div
+                className={tremorTwMerge(
+                  makeBarListClassName("bar"),
+                  // common
+                  "flex items-center rounded-tremor-small bg-opacity-30",
+                  rowHeight,
+                  item.color || color
+                    ? getColorClassNames(item.color ?? (color as Color), colorPalette.background)
+                        .bgColor
+                    : "bg-tremor-brand-subtle dark:bg-dark-tremor-brand-subtle dark:bg-opacity-30",
+                  idx === data.length - 1 ? spacing.none.marginBottom : spacing.sm.marginBottom,
+                )}
+                style={{
+                  width: `${widths[idx]}%`,
+                  transition: showAnimation ? "all 1s" : "",
+                }}
+              >
+                <div
+                  className={tremorTwMerge(
+                    "absolute max-w-full flex",
+                    // spacing separate/not separate value
+                    separateValue ? spacing.sm.left : spacing.lg.left,
+                  )}
+                >
+                  {Icon ? (
+                    <Icon
+                      className={tremorTwMerge(
+                        makeBarListClassName("barIcon"),
+                        // common
+                        "flex-none",
+                        // light
+                        "text-tremor-content",
+                        // dark
+                        "dark:text-dark-tremor-content",
+                        sizing.lg.height,
+                        sizing.lg.width,
+                        spacing.md.marginRight,
+                      )}
+                    />
+                  ) : null}
+                  {item.href ? (
+                    <a
+                      href={item.href}
+                      target={item.target ?? "_blank"}
+                      rel="noreferrer"
+                      className={tremorTwMerge(
+                        makeBarListClassName("barLink"),
+                        // common
+                        "whitespace-nowrap hover:underline truncate text-tremor-default",
+                        // light
+                        "text-tremor-content-emphasis",
+                        // dark
+                        "dark:text-dark-tremor-content-emphasis",
+                        // not separate value
+                        !separateValue && "max-w-[85%]",
+                      )}
+                    >
+                      {item.name}
+                    </a>
+                  ) : (
+                    <p
+                      className={tremorTwMerge(
+                        makeBarListClassName("barText"),
+                        // common
+                        "whitespace-nowrap truncate text-tremor-default",
+                        // light
+                        "text-tremor-content-emphasis",
+                        // dark
+                        "dark:text-dark-tremor-content-emphasis",
+                        // not separate value
+                        !separateValue && "max-w-[85%]",
+                      )}
+                    >
+                      {item.name}
+                    </p>
+                  )}
+                </div>
+                {!separateValue && (
+                  <div className={tremorTwMerge("absolute max-w-full flex right-0")}>
+                    <p
+                      className={tremorTwMerge(
+                        makeBarListClassName("barTextRight"),
+                        // common
+                        "whitespace-nowrap truncate text-tremor-default",
+                        // light
+                        "text-tremor-content-emphasis",
+                        // dark
+                        "dark:text-dark-tremor-content-emphasis",
+                      )}
+                    >
+                      {valueFormatter(item.value)}
+                    </p>
+                  </div>
                 )}
               </div>
-            </div>
+              {showDivide && idx < data.length - 1 && (
+                <hr className="h-px my-4 bg-[#E4E6EA] border-0" />
+              )}
+            </Fragment>
           );
         })}
       </div>
-      <div className={(makeBarListClassName("labels"), "text-right min-w-min")}>
-        {data.map((item, idx) => (
-          <div
-            key={item.key ?? item.name}
-            className={tremorTwMerge(
-              makeBarListClassName("labelWrapper"),
-              "flex justify-end items-center",
-              rowHeight,
-              idx === data.length - 1 ? spacing.none.marginBottom : spacing.sm.marginBottom,
-            )}
-          >
-            <p
+      {separateValue && (
+        <div className={(makeBarListClassName("labels"), "text-right min-w-min")}>
+          {data.map((item, idx) => (
+            <div
+              key={item.key ?? item.name}
               className={tremorTwMerge(
-                makeBarListClassName("labelText"),
-                // common
-                "whitespace-nowrap truncate text-tremor-default",
-                // light
-                "text-tremor-content-emphasis",
-                // dark
-                "dark:text-dark-tremor-content-emphasis",
+                makeBarListClassName("labelWrapper"),
+                "flex justify-end items-center",
+                rowHeight,
+                idx === data.length - 1 ? spacing.none.marginBottom : spacing.sm.marginBottom,
+                showDivide && idx < data.length - 1 && "mb-[33px]",
               )}
             >
-              {valueFormatter(item.value)}
-            </p>
-          </div>
-        ))}
-      </div>
+              <p
+                className={tremorTwMerge(
+                  makeBarListClassName("labelText"),
+                  // common
+                  "whitespace-nowrap truncate text-tremor-default",
+                  // light
+                  "text-tremor-content-emphasis",
+                  // dark
+                  "dark:text-dark-tremor-content-emphasis",
+                )}
+              >
+                {valueFormatter(item.value)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 });
